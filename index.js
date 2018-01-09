bbd1f528766f95b5a19946500757da2622743199
Tim Jordan <raeyejordan@yahoo.com>
2018-01-09 05:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you"
   }
-  if (string.toUpperCase === string) {
+  if (string.toUpperCase() === string) {
     return "YES INDEED"
   }
   if (string === "I love you, Grandma") {
     return "I love you, too!"
   }
-  return "Are you eating enough?"
 }
