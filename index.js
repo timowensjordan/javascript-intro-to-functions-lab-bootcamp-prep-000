2f5b5a5d3ee8499b8a31d64113500014db3f2729
Tim Jordan <raeyejordan@yahoo.com>
2018-01-09 05:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
     return "I cant hear you!"
   }
-  if(string.toUpperCase() ===string) {
+  if(string.toUpperCase() === string) {
     return "YES INDEED"
   }
   if (string === "I love you, Grandma") {
